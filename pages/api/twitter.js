954c33bf20a04b484a8216447e26a58849631e24
Robert Dominguez <rtjdominguez@gmail.com>
2021-10-21 10:44:18 -0500

    debugging still

--- pages/api/twitter.js
+++ pages/api/twitter.js
@@ -9,6 +9,7 @@ const client = new twitter({
 export default async (req, res) => {
   let testText = `Hello, there!`;
   let testUrl = `https://www.dominguezdev.com/blog/qr-carpool`;
+  console.log(req.body.data);
   await client
     .post("statuses/update", {
       status: `${req.body.data.tweetText} https://dominguezdev.com/blog/${req.body.data.slug}`,
